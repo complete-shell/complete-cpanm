2d77c393b8176bad8dacd833ee0838a09e1786b4
Ingy döt Net <ingy@ingy.net>
2019-06-18 18:55:22 -0400

    Make pass shellcheck

--- cpanm.bash
+++ cpanm.bash
@@ -1,9 +1,6 @@
-cpan-module() {
-  command -v cpanm >/dev/null || {
-    hint "Can't find cpanm home directory. Set PERL_CPANM_HOME."
-    return 0
-  }
+# shellcheck shell=bash disable=2034
 
+cpan-module() {
   modules=$COMPLETE_SHELL_CACHE/cpanm
   [[ -f $modules ]] || {
     hint "Can't find CPAN modules cache file: '$modules'."
@@ -12,11 +9,11 @@ cpan-module() {
   }
 
   local fzf_options=(
-    --layout=reverse
-    --multi
-    --height=0
-    --color=bw
-    --bind=space:toggle-down,ctrl-space:up+toggle,del:deselect-all+top
+    '--layout=reverse'
+    '--multi'
+    '--height=0'
+    '--color=bw'
+    '--bind=space:toggle-down,ctrl-space:up+toggle,del:deselect-all+top'
   )
 
   (echo; cat "$modules") | complete-shell-pager
